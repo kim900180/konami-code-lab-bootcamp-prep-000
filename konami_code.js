abd3a82d4a8cc8b24e36f71d29517872811946b7
Jae Yong Kim <kim900180@gmail.com>
2018-07-13 01:17:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,63 @@ const codes = [
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(e) {
+  const key = e.key;
+  var index = 0
+
+  if (key === codes[index]) {
+    index ++
+    if (key === codes[index]) {
+      index ++
+      if (key === codes[index]) {
+        index ++
+        if (key === codes[index]) {
+          index ++
+          if (key === codes[index]) {
+            index ++
+            if (key === codes[index]) {
+              index ++
+              if (key === codes[index]) {
+                index ++
+                if (key === codes[index]) {
+                  index ++
+                  if (key === codes[index]) {
+                    index ++
+                    if (key === codes[index]) {
+                      index ++
+                      if (index === codes.length) {
+                        alert('hurray');
+                        index = 0
+                      }
+                    } else {
+                      index = 0
+                    }
+                  } else {
+                    index = 0
+                  }
+                } else {
+                  index = 0
+                }
+              } else {
+                index = 0
+              }
+            } else {
+              index = 0
+            }
+          } else {
+            index = 0
+          }
+        } else {
+          index = 0
+        }
+      } else {
+        index = 0
+      }
+    } else {
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+  })
 }
