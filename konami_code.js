8fb84310f865bc354399d3bd1ed0b053eb68ba37
Jae Yong Kim <kim900180@gmail.com>
2018-07-13 01:25:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,12 +14,9 @@ const codes = [
 function init() {
   // your code here
   document.body.addEventListener('keydown', function(e) {
-  const key = e.key;
-  var index = 0
-  
-  for (var i = 0; i < codes.length; i++) 
-    if (e.key === codes[i]) {
-      alert('hurray')
-    }
+    const key = e.key;
+    var index = 0
+    
+    do 
   })
 }
